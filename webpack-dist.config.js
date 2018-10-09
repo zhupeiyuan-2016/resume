77ab2a8bbb15dcb8d23f3fde43e01af5ce68d5ee
jiumi <jiumilove@163.com>
2018-10-09 16:40:24 +0800

    updata

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -96,14 +96,14 @@ module.exports = {
     }),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'www.uooou.cn');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'readme.uooou.cn');
 
       await publishGhPages();
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://www.uooou.cn' // 这里注意改成你的在线简历的网站
+        'http://readme.uooou.cn' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
